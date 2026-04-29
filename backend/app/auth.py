01a0f481d0fa4d52cdc47db5be6a64b08ca89f71
Jewel Jireh Rosario <jjrosario67@gmail.com>
2026-04-29 10:33:14 +0800

    feat: add JWT authentication and protected route

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -3,12 +3,17 @@
 from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
 
 SECRET_KEY = "change-this-secret-key-later"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def hash_password(password: str):
@@ -26,3 +31,17 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("user_id")
+
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+        return user_id
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 # backend/app/main.py
 
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
 from app.database import Base, engine
 from app.routes import users
+from app.auth import get_current_user
 
 Base.metadata.create_all(bind=engine)
 
@@ -15,3 +15,11 @@ app.include_router(users.router)
 @app.get("/")
 def read_root():
     return {"message": "Personal Tracker API is running"}
+
+
+@app.get("/protected")
+def protected_route(user_id: int = Depends(get_current_user)):
+    return {
+        "message": "You are authenticated",
+        "user_id": user_id
+    }
